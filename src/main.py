ad6f71b855144dfba80dbcd69d289c0c95de60b2
nicholasutikal <nicholasutikal@gmail.com>
2022-02-24 19:21:50 +0100

    applies fixes to main file

--- src/main.py
+++ src/main.py
@@ -25,6 +25,8 @@ def set_chrome_options() -> None:
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+    chrome_options.add_argument(f'user-agent={user_agent}')
     chrome_prefs = {"download.default_directory": r"{}".format(str(path))}
     # chrome_prefs = {}
     chrome_options.experimental_options["prefs"] = chrome_prefs
@@ -83,6 +85,7 @@ def main() -> None:
 
     driver = webdriver.Chrome(chrome_options=set_chrome_options())
     driver.get('https://footystats.org/login')
+    driver.get_screenshot_as_file("screenshot.png")
 
     time.sleep(5)
     print(driver)
